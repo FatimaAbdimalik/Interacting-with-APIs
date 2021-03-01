4d8b38a8d460b30666925065fcd2069d49a6e01f
Fatima Abdimalik <f.abdimalik@gmail.com>
2021-03-01 16:07:30 +0000

    better way of getting token

--- src/components/FindArtist.js
+++ src/components/FindArtist.js
@@ -3,18 +3,19 @@ import GetAlbum from "./GetAlbum";
 const FindArtist = ({ value }) => {
   console.log(value);
   const [token, setToken] = useState(
-    "BQCh3ilfMLHmh38W0_tWCHDbtz_FOtro-cbMM4KArRy_huoU2P6fMX1tSnx9D8azcP9Sp2XdxWmw9QP-VCzOrT5NYbmoSyhZpkoMEREbhKhirZehsJFbqtfAPOfGa2y2U5bT5qP2Sk2PRBqyG2nC99wcZ3DQMUcZ8CGx"
+    // "BQCh3ilfMLHmh38W0_tWCHDbtz_FOtro-cbMM4KArRy_huoU2P6fMX1tSnx9D8azcP9Sp2XdxWmw9QP-VCzOrT5NYbmoSyhZpkoMEREbhKhirZehsJFbqtfAPOfGa2y2U5bT5qP2Sk2PRBqyG2nC99wcZ3DQMUcZ8CGx"
+    ""
   );
   const [artist, setArtist] = useState();
 
-  // useEffect(() => {
-  //   fetch(`https://spotify-api-feb.herokuapp.com/token`)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log(data.access_token);
-  //       setToken(data.access_token);
-  //     });
-  // }, []);
+  useEffect(() => {
+    fetch(`https://spotify-api-feb.herokuapp.com/token`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.access_token);
+        setToken(data.access_token);
+      });
+  }, []);
 
   useEffect(() => {
     if (token) {
